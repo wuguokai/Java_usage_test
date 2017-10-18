ecd89c29300a92c5296b61bf3504f588bf64ae54
WUGUOKAI <guokai.wu@hand-china.com>
2017-10-18 17:09:30 +0800

    修改了读取命令行的执行过程process的状态

--- src/com/testbat/TestCmd.java
+++ src/com/testbat/TestCmd.java
@@ -7,20 +7,29 @@ import java.io.InputStreamReader;
 
 public class TestCmd {
     public static void main(String[] args) {
-        String path = "C:\\Users\\WUGUOKAI\\Desktop\\package.bat";
+        String path = "jarsigner -verbose -keystore C:\\Users\\WUGUOKAI\\Desktop\\MobileCloud.jks -storepass handhand -signedjar C:\\Users\\WUGUOKAI\\Desktop\\MobileCloud.apk  C:\\Users\\WUGUOKAI\\Desktop\\app-release.apk MobileCloud";
 //        String path = "jarsigner -verbose -keystore C:\\Users\\WUGUOKAI\\Desktop\\test.jks -storepass 123456 -signedjar C:\\Users\\WUGUOKAI\\Desktop\\test1.apk -digestalg SHA1 -sigalg MD5withRSA C:\\Users\\WUGUOKAI\\Desktop\\app-release.apk test";
         Runtime run = Runtime.getRuntime();
         try {
 //            Process process = run.exec("cmd /k start" + path);
             Process process = run.exec(path);
 //            run.exec("cmd.exe /k d:");
-            InputStream in = process.getInputStream();
-            BufferedReader strCon = new BufferedReader(new InputStreamReader(in));
-            String line;
-            while ((line = strCon.readLine()) != null) {
-                System.out.println(line);
+            //取得命令结果的输出流
+            InputStream fis=process.getInputStream();
+            //用一个读输出流类去读
+            InputStreamReader isr=new InputStreamReader(fis);
+            //用缓冲器读行
+            BufferedReader br=new BufferedReader(isr);
+            String line=null;
+            //直到读完为止
+            while((line=br.readLine())!=null)
+            {
+//                System.out.println(line);
+                System.out.println(process.isAlive());
             }
-            process.waitFor();
+            System.out.println(process.isAlive());
+           /* process.waitFor();
+            System.out.println("111")*/;
         } catch (Exception e) {
             e.printStackTrace();
         }
